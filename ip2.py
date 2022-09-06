22d2dfe2066bcab57865a1e78dcbb4239cad642e
dicky.surachman1@gmail.com <dicky.surachman1@gmail.com>
2022-09-07 01:33:35 +0700

    data

--- ip2.py
+++ ip2.py
@@ -2,10 +2,11 @@ import socket
 TCP_IP='172.16.20.8'
 TCP_PORT=25001
 BUFFER_SIZE=1024
+from urllib import request
 #MESSAGE='00 00 00 00 00 29 00 64 00 25 00 cf 00 00 00 00 13 31 32 31 39 30 32 38 30 31 38 32 30 39 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'
 #MESSAGE='00 00 00 00 00 26 00 64 00 22 00 cf 00 00 00 00 05 62 4d 49 49 31 03 43 43 43 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'
 #MESSAGE='00 00 00 00 00 26 00 64 00 22 00 cf 00 00 00 00 05 42 4d 49 49 31 03 43 43 43 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'
-MESSAGE='00 00 00 00 00 26 00 64 00 22 00 cf 00 00 00 00 05 44 49 43 4b 59 03 41 4a 41 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'
+MESSAGE='00 00 00 00 00 06 00 64 00 02 00 45'
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((TCP_IP,TCP_PORT))
@@ -14,3 +15,7 @@ data=s.recv(BUFFER_SIZE)
 s.close()
 print("receiveddata:")
 print(data)
+data=str(data)
+data=data.replace(" ","")
+url1 = "http://localhost/printer/site/camera.html?status="+str(data)
+response2 = request.urlopen(url1)
