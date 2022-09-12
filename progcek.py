719ab86991bfff266c81725906b714eb8c10934e
dicky.surachman1@gmail.com <dicky.surachman1@gmail.com>
2022-09-12 16:53:53 +0700

    program counter

--- progcek.py
+++ progcek.py
@@ -2,6 +2,7 @@
 
 import json
 import socket
+import binascii
 from urllib import request
 from time import time, sleep
 file1 = open('setting.txt', 'r')
@@ -59,10 +60,18 @@ while True:
                 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                 s.connect((str(TCP_IP),int(TCP_PORT)))
                 #cek hitung pertama
-                printd="00 00 00 00 00 06 00 64 00 02 00 A7"                
+                printd="00 00 00 00 00 06 00 64 00 02 00 45"                
                 s.send(bytes.fromhex(printd))
                 data1=s.recv(int(BUFFER_SIZE))
-                awal=int(data1[70:74],16)
+                data2=binascii.hexlify(data1).decode()
+                try :
+                    awal=int(data2[70:74],16)
+                except :
+                    print ("koneksi error")
+                    url1 = str(settt[1])+"?id="+str(i['id'])+"&status=failure&logbaca=koneksierror"+str(baca.replace(" ",""))
+                    response2 = request.urlopen(url1)
+                    print(str(y) + " sending to server " +url1)   
+                    break                     
                 print("counter printer pertama ",awal)
                 #send data printer
                 s.send(bytes.fromhex(MESSAGE))
@@ -75,13 +84,16 @@ while True:
                 #cek counter data
                 s.send(bytes.fromhex(printd))
                 data1=s.recv(int(BUFFER_SIZE))
-                akhir=int(data1[70:74],16)
+                data2=binascii.hexlify(data1).decode()
+                akhir=int(data2[70:74],16)
                 print("counter printer akhir ",akhir)
-                while awal>akhir:
+                while awal>=akhir:
                         s.send(bytes.fromhex(printd))
                         data1=s.recv(int(BUFFER_SIZE))
-                        akhir=int(data1[70:74],16)
+                        data2=binascii.hexlify(data1).decode()
+                        akhir=int(data2[70:74],16)
                         print("counter printer akhir ",akhir)
+                        sleep(int(settt[2]) - time() % int(settt[2]))
                 if(x1>0): 
                     print("data berhasil diterima")
                     url1 = str(settt[1])+"?id="+str(i['id'])+"&status=sukses"
