7103da2bcf6106213aa2483cd5638ccc5c65a29c
dicky.surachman1@gmail.com <dicky.surachman1@gmail.com>
2022-09-16 13:49:18 +0700

    job update

--- advanced/frontend/views/item/index.php
+++ advanced/frontend/views/item/index.php
@@ -46,17 +46,17 @@ CrudAsset::register($this);
                 'heading' => '<i class="fa fa-list"></i> <b>'.$this->title.'</b>',
                 'before'=>'<em>* '.Yii::t('yii2-ajaxcrud', 'Resize Column').'</em>',
                 'after'=>BulkButtonWidget::widget([
-                            'buttons'=>Html::a('<i class="fa fa-trash"></i>&nbsp; '.Yii::t('yii2-ajaxcrud', 'Running All'),
-                                ["runningdelete"] ,
+                            'buttons'=>Html::a('<i class="fa fa-trash"></i>&nbsp; '.Yii::t('yii2-ajaxcrud', 'Delete All'),
+                                ["bulkdelete"] ,
                                 [
                                     "class"=>"btn btn-danger btn-xs",
                                     'role'=>'modal-remote-bulk',
                                     'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
                                     'data-request-method'=>'post',
-                                    'data-confirm-title' => Yii::t('yii2-ajaxcrud', 'Running'),
-                                    'data-confirm-message' => Yii::t('yii2-ajaxcrud', 'Running Confirm')
+                                    'data-confirm-title' => Yii::t('yii2-ajaxcrud', 'Delete'),
+                                    'data-confirm-message' => Yii::t('yii2-ajaxcrud', 'Delete Confirm')
                                 ]),
-                        ]).                        
+                        ]).                       
                         '<div class="clearfix"></div>',
             ]
         ])?>
@@ -75,3 +75,20 @@ CrudAsset::register($this);
     ]
 ])?>
 <?php Modal::end(); ?>
+<?php /*
+
+BulkButtonWidget::widget([
+                            'buttons'=>Html::a('<i class="fa fa-trash"></i>&nbsp; '.Yii::t('yii2-ajaxcrud', 'Running All'),
+                                ["runningdelete"] ,
+                                [
+                                    "class"=>"btn btn-danger btn-xs",
+                                    'role'=>'modal-remote-bulk',
+                                    'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
+                                    'data-request-method'=>'post',
+                                    'data-confirm-title' => Yii::t('yii2-ajaxcrud', 'Running'),
+                                    'data-confirm-message' => Yii::t('yii2-ajaxcrud', 'Running Confirm')
+                                ]),
+                        ]). 
+
+
+                        */ ?>
