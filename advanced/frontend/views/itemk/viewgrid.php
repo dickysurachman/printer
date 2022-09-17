96af48ddac3fe0ff3b11c23a8c73af3da20fde77
dicky.surachman1@gmail.com <dicky.surachman1@gmail.com>
2022-09-17 21:44:09 +0700

    update

--- advanced/frontend/views/itemk/viewgrid.php
+++ advanced/frontend/views/itemk/viewgrid.php
@@ -20,13 +20,14 @@ use yii\helpers\Html;
         <?php $i=1;
     foreach($model->detail as $value){
         //Html::a('<span class="fas fa-barcode" style="font-size:10pt;" title="Start Input"></span> '.\Yii::t('yii', 'LAN'), ['itemk/view', 'id' => $value->itemd->id], ['data-pjax' => "0",'class'=>'btn btn-danger','target'=>"_blank"])."&nbsp;&nbsp;".
+        if(isset($value->itemd)){
         echo "<tr><td>".$i."</td>";
         echo "<td>".(isset($value->itemd)?$value->itemd->var_1:'')."</td>";
         echo "<td>".(isset($value->itemd)?$value->itemd->var_2:'')."</td>";
         echo "<td>".(isset($value->itemd)?$value->itemd->var_3:'')."</td>";
-        echo "<td>".(isset($value->itemd)?$value->itemd->var_5:'')."</td></tr>";
-       
+        echo "<td>".(isset($value->itemd)?$value->itemd->var_5:'')."</td></tr>";       
         $i++; 
+        }
       }
         ?>
       </table>
