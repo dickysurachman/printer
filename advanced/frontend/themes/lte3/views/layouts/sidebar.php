439f9536e05c8a2d135e2b330478a884e7e15701
dicky.surachman1@gmail.com <dicky.surachman1@gmail.com>
2022-10-01 14:37:04 +0700

    update:

--- advanced/frontend/themes/lte3/views/layouts/sidebar.php
+++ advanced/frontend/themes/lte3/views/layouts/sidebar.php
@@ -18,7 +18,9 @@ use hscstudio\mimin\components\Mimin;
                 <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block"><?php 
+                if(!\Yii::$app->user->isGuest) echo \Yii::$app->user->identity->username;
+                ?></a>
             </div>
         </div>
 
