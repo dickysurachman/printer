f87734dc4e5e876e9b81af156e447793350602d9
dicky.surachman1@gmail.com <dicky.surachman1@gmail.com>
2023-10-30 11:15:08 +0700

    update expired

--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -1,6 +1,6 @@
 <?php
 $ss=date("Y-m-d");
-$ex=date("Y-m-d",strtotime("2023-12-22"));
+$ex=date("Y-m-d",strtotime("2024-12-22"));
 if(strtotime($ss)>=strtotime($ex)){
     echo "Time Expired";
     die();
